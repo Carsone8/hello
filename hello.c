25992e13c639ad43dd4a645d04bb41165044b058
Carsone8 <carson.eckhardt@icloud.com>
2026-08-27 09:52:40 -0500

    added major to output

--- hello.c
+++ hello.c
@@ -1,9 +1,13 @@
+
+
+
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
 
-  printf("Carson\n");
+  printf("I'm Carson Eckhardt!\n");
+  printf("My major is Computer Science\n");
 
   return 0;
 }
